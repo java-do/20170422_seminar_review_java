d416c1c14f22cc85596991545e6f9e339459d216
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2017-09-02 10:15:43 +0900

    ハンズオン用のファイルを追加

---
+++ src/test/java/com/example/handon/HandsOn8.java
@@ -0,0 +1,89 @@
+package com.example.handon;
+
+import org.junit.Test;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class HandsOn8 {
+
+	private List<String> words = Arrays.asList("Hello", "Hi", "Bye", "Good Night");
+
+	@Test
+	public void 今日の日付と時間と日時を作る() {
+
+	}
+
+	@Test
+	public void 日時の表示方法を加工する() {
+
+	}
+
+	@Test
+	public void 日時を操作する() {
+
+	}
+
+	@Test
+	public void 日時の差を計算する() {
+
+	}
+
+
+	@Test
+	public void Streamとこれまでの違い() {
+		
+	}
+
+	@Test
+	public void 長さが5以上の単語だけをListに集める() {
+
+	}
+
+	@Test
+	public void 要素を単語の長さに変えてListに集める() {
+
+	}
+
+	@Test
+	public void 要素を単語の長さに変えて5以上の数字だけをListに集める() {
+
+	}
+
+	@Test
+	public void 要素を文字のリストに変えてListに集める() {
+
+	}
+
+	@Test
+	public void 単語と文字数のMapに集める() {
+
+	}
+
+	@Test
+	public void 一番短い単語を取得する() {
+
+	}
+
+	@Test
+	public void 一番長い単語を取得する() {
+
+	}
+
+	@Test
+	public void 単語の長さを合計する() {
+
+	}
+
+	@Test
+	public void 異なる処理をするStreamを合成して実行する() {
+
+	}
+
+}
